31f8ac092b1403949411752c78feff16b73d2480
Robert <chen1fan@icloud.com>
2020-04-07 05:01:53 -0400

    Push for Tuesday Class

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 offset;
+    public Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset =  transform.position - Player.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.position + offset;
+    }
+}
