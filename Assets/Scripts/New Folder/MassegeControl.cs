52972734f49862179845e6e9e93b2c4c9da954ac
zhaozehua <zz1791@nyu.edu>
2020-04-22 21:48:15 +0800

    typewriter and terrain

--- Assets/Scripts/New Folder/MassegeControl.cs
+++ Assets/Scripts/New Folder/MassegeControl.cs
@@ -19,41 +19,48 @@ public class MassegeControl : MonoBehaviour
 
     public int currentLine;//the line counter
     public int endLineAt;//the number for the last line
+
+    private float typeWriterDelay = 0.02f;
+    public string fullText;
+    public string currentText;
     
 
     void Start()
     {
         messageCanvas.enabled = false;//turn off canvas at the beginning
         textLines = (textFile1.text.Split('\n'));//splitting the text file in to lines
-        
+
         if (endLineAt == 0)
         {
             endLineAt = textLines.Length - 1; // set the number for the last line by how many lines the file has
         }
-
+        
+        
     }
 
 
      void Update()
      {
-
-         massage1.text = textLines[currentLine];
-
-         if (Input.GetKeyDown(KeyCode.Space))
-         {
-             currentLine += 1;
-         }
-
-         if (currentLine > endLineAt)
-         {
-             messageCanvas.enabled = false;
-             currentLine = 0;
-         }
          
          if( messageCanvas.enabled == true)
          {
              Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
              textBoxContainer.transform.position = screenPos;
+
+             //massage1.text = textLines[currentLine];
+
+             if (Input.GetKeyDown(KeyCode.Space))
+             {
+                 StartCoroutine(TypeWriter());
+                 currentLine += 1;
+                 fullText = textLines[currentLine];
+             }
+
+             if (currentLine > endLineAt - 1)
+             {
+                 messageCanvas.enabled = false;
+                 currentLine = 0;
+             }
          }
          
 
@@ -67,15 +74,14 @@ public class MassegeControl : MonoBehaviour
                 TurnOnMessage();  // turn on canvas when Claire enters the collider
                 
                 Debug.Log("entered");
-//                Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-//                textBoxContainer.transform.position = screenPos;
-
+                StartCoroutine(TypeWriter());
+                currentLine = 1;
          }
      }
  
     private void TurnOnMessage()
     {
-        messageCanvas.enabled = true;  //function for enable canvas
+        messageCanvas.enabled = true;//function for enable canvas
     }
          
     void OnTriggerExit(Collider other)
@@ -90,6 +96,16 @@ public class MassegeControl : MonoBehaviour
     private void TurnOffMessage()
     {
         messageCanvas.enabled = false; //function for disable the canvas
+    }
+
+    private IEnumerator TypeWriter()
+    {
+        for (int i = 0; i < fullText.Length; i ++ )
+        {
+            currentText = fullText.Substring(0,i);
+            massage1.text = currentText;
+            yield return new WaitForSeconds(typeWriterDelay);
+        }
     }
 
 }
