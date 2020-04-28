12a9602538261bba8c935c4d9fe12e4b94beda1a
Robert <chen1fan@icloud.com>
2020-04-28 03:25:17 -0400

    add cinemachine camera control and basic terrain

---
+++ Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    private Animator CameraAnimator;
+    public int triggerState;
+    // Start is called before the first frame update
+    void Start()
+    {
+       CameraAnimator =  GameObject.Find("playerBody").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Claire")
+        {
+            CameraAnimator.SetInteger("CameraState",triggerState );
+        }
+    }
+}
