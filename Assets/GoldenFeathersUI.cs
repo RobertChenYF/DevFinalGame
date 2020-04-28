d11565e56b1744543f064c416dd4470db6c43b45
FINNJC <finnjcarney@gmail.com>
2020-04-28 19:41:55 -0300

    UI System for golden feathers is implimented in my scene, going up to 8 golden feathers with the ability to add more if needed.  Textures still needed for feathers.

---
+++ Assets/GoldenFeathersUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenFeathersUI : MonoBehaviour
+{
+
+    public GoldenFeather[] goldenFeathers;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < goldenFeathers.Length; i++)
+        {
+            if(ClaireController.me.goldenFeathersMax > i)
+            {
+                goldenFeathers[i].dead.color = Color.white;
+            }
+            else
+            {
+                goldenFeathers[i].dead.color = Color.clear;
+            }
+
+            if (ClaireController.me.goldenFeathers < i)
+            {
+                goldenFeathers[i].golden.color = Color.clear;
+            }
+            else if(ClaireController.me.goldenFeathers > i + 1)
+            {
+                goldenFeathers[i].golden.color = Color.white;
+            }
+            else
+            {
+                goldenFeathers[i].golden.color = new Color(1, 1, 1, ClaireController.me.goldenFeathers - i);
+            }
+
+
+        }
+    }
+}
+
+[System.Serializable]
+public struct GoldenFeather
+{
+    public SpriteRenderer golden;
+    public SpriteRenderer dead;
+}
