f4b93b5421d5f0b07c8f3423fe5c46628cd9cb4b
Robert <chen1fan@icloud.com>
2020-05-04 17:36:37 -0400

    fix cape color

--- Assets/Scripts/ClaireAnimatorController.cs
+++ Assets/Scripts/ClaireAnimatorController.cs
@@ -20,7 +20,7 @@ public class ClaireAnimatorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        CapeMaterial.SetColor("ColorOfCape", blue);
     }
 
     // Update is called once per frame
