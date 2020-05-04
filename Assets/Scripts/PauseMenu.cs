e969934e1b9ed8b4b8f3219dc4e41a8babcaee26
Robert <chen1fan@icloud.com>
2020-05-04 17:17:14 -0400

    add UI input with controller push to build the project

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -13,7 +13,7 @@ public class PauseMenu : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetButtonDown("Menu"))
         {
             if (gameIsPaused)
             {
